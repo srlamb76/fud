ad448d8290e33b9f75ba012e25c2696edfe98479
srlamb76 <moremachinecodethanman@gmail.com>
2020-01-06 10:30:54 +1100

    Add files via upload
    Initial project setup via lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fud
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fud/core_test.clj
@@ -0,0 +1,7 @@
+(ns fud.core-test
+  (:require [clojure.test :refer :all]
+            [fud.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
